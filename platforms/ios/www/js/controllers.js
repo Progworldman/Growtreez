f61f8eaeabfd013a446810ec61269ca953ad2307
Progworldman <andrey19880802@gmail.com>
2016-04-08 02:06:38 +0800

    - App Name/Icon Changed - Functionality zoom in/out of landing page progress Now couldn’t do panning and zooming also changing the view when certain  level of zooming is achieved which is the key point

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -3,18 +3,47 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  var width = 960,
+    height = 500;
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+var randomX = d3.random.normal(width / 2, 80),
+    randomY = d3.random.normal(height / 2, 80);
+
+var data = d3.range(2000).map(function() {
+  return [
+    randomX(),
+    randomY()
+  ];
+});
+
+var canvas = d3.select("ion-content").append("canvas")
+    .attr("width", width)
+    .attr("height", height)
+    .call(d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", zoom))
+  .node().getContext("2d");
+
+draw();
+
+function zoom() {
+  canvas.save();
+  canvas.clearRect(0, 0, width, height);
+  canvas.translate(d3.event.translate[0], d3.event.translate[1]);
+  canvas.scale(d3.event.scale, d3.event.scale);
+  draw();
+  canvas.restore();
+}
+
+function draw() {
+  var i = -1, n = data.length, d;
+  canvas.beginPath();
+  while (++i < n) {
+    d = data[i];
+    canvas.moveTo(d[0], d[1]);
+    canvas.arc(d[0], d[1], 2.5, 0, 2 * Math.PI);
+  }
+  canvas.fill();
+}
+
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
